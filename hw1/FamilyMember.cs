cedf921cef28fb0a7dc8b3fec94044a2a05915e9
Andrey <ab18021999@mail.ru>
2024-01-05 03:53:16 +0500

    Добавьте файлы проекта.

---
+++ hw1/FamilyMember.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw1
+{
+    enum Gender { Male, Female }
+    class FamilyMember
+    {
+        public string? Name { get; set; }
+        public Gender gender { get; set; }
+        public FamilyMember[]? Children { get; set; }
+        public FamilyMember? Mother { get; set; }
+        public FamilyMember? Father { get; set; }
+
+        public FamilyMember()
+        {
+        }
+
+        public FamilyMember(string name, Gender gender, FamilyMember mother, FamilyMember father, params FamilyMember[] children)
+        {
+            this.Name = name;
+            this.gender = gender;
+            this.Mother = mother;
+            this.Father = father;
+            this.Children = children;
+        }
+
+        public FamilyMember FindOldMather(FamilyMember member)
+        {
+            while (member.Mother != null)
+            {
+                member = member.Mother;
+            }
+            return member;
+        }
+        public void MotherInFamaly()
+        {
+            FamilyMember adult = this;
+            if (adult.Mother != null)
+            {
+                adult = adult.Children[0]?.Mother;
+
+            }
+            adult = FindOldMather(adult);
+
+
+            Console.WriteLine($"{adult.Name} --> ");
+
+            bool femaleChild = true;
+            while (true)
+            {
+                femaleChild = false;
+                Console.WriteLine($"{adult.Name} --> ");
+
+                foreach (FamilyMember child in adult.Children)
+                {
+                    if (child.gender == Gender.Female)
+                    {
+                        adult = child;
+                        femaleChild = true;
+                        break;
+                    }
+                }
+
+            }
+
+
+
+
+        }
+
+        public void ShowThree()
+        {
+            List<List<FamilyMember>> levels = new List<List<FamilyMember>>();
+            List<FamilyMember> currentLevel = new List<FamilyMember> { this };
+          
+            do {
+                List<FamilyMember> level = new List<FamilyMember>();
+                List<FamilyMember> tmpCurrentlevel = new List<FamilyMember>();
+                foreach (FamilyMember mem in currentLevel)
+                {
+                    if (mem.Children != null)
+                    {
+                        foreach (FamilyMember child in mem.Children)
+                        {
+                            if (child.Mother != null)
+                            {
+                                if (!level.Contains(child.Mother))
+                                {
+                                    level.Add(child.Mother);
+                                }
+                            }
+                            if (child.Father != null)
+                            {
+                                if (!level.Contains(child.Father))
+                                {
+                                    level.Add(child.Father);
+                                }
+                            }
+                            tmpCurrentlevel.Add(child);
+
+                        }
+                    }
+                    else
+                    {
+                        level.Add(mem);
+                    }
+                }
+                
+                levels.Add(level);
+                currentLevel = tmpCurrentlevel;
+            } 
+            while (currentLevel.Count > 0);
+            
+            PrintTree( levels );
+
+        }
+
+        private void PrintTree(List<List<FamilyMember>> levels)
+        {
+            foreach (List<FamilyMember> level in levels)
+            {
+
+                foreach (FamilyMember member in level)
+                {
+                    Console.Write($"{member.Name} ");
+                }
+                Console.WriteLine();
+            }
+        }
+       
+
+
+       
+    }
+}
---
+++ hw1/Program.cs
@@ -0,0 +1,28 @@
+﻿using hw1;
+
+class Program
+{
+    public static void Main()
+    {
+        FamilyMember grandfather = new() { Mother = null, Father = null, Name = "Дедушка", gender = Gender.Male };
+        FamilyMember grandmother = new() { Mother = null, Father = null, Name = "Бабушка", gender = Gender.Female };
+        FamilyMember father = new FamilyMember() { Mother = grandmother, Father = grandfather, Name = "Папа", gender = Gender.Male };
+        FamilyMember brother = new FamilyMember() { Mother = grandmother, Father = grandfather, Name = "Брат", gender = Gender.Male };
+        grandfather.Children = new FamilyMember[] { father, brother };
+        FamilyMember mother = new FamilyMember() { Mother = null, Father = null, Name = "Мама", gender = Gender.Female };
+        FamilyMember mother2 = new FamilyMember() { Father = null, Mother = null, Name = "Мама2", gender = Gender.Female };
+        FamilyMember son = new FamilyMember() { Mother = mother, Father = father, Name = "Сын", gender = Gender.Male };
+        FamilyMember son2 = new FamilyMember() { Father = father, Mother = mother2, Name = "Сын2", gender = Gender.Male };
+        mother2.Children = new FamilyMember[] { son2 };
+
+
+        mother.Children = new FamilyMember[] { son };
+        father.Children = new FamilyMember[] { son, son2 };
+
+
+
+        grandfather.ShowThree();
+
+    }
+
+}
