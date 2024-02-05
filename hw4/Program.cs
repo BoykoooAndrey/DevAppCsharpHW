1280101cfcd0671465fe985a02146029a45b6e26
Andrey <ab18021999@mail.ru>
2024-02-05 12:46:36 +0500

    hw4 ready

---
+++ hw4/Program.cs
@@ -0,0 +1,47 @@
+﻿class Programm
+{
+    static void Main(string[] args)
+    {
+		int[] arr = new int[] { 1, 33, 33, 26, 40, 33, 7, 10 };
+
+		int target = 99;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+			
+			var x = target - arr[i];
+		
+			HashSet<int> set = new HashSet<int>();
+			for (int j = 0; j < arr.Length; j++)
+			{
+                if (i != j)
+                {
+					int y = x - arr[j];
+					if (set.Contains(y))
+					{
+						Console.WriteLine($"{arr[i]} + {arr[j]} + {y} = {target}");
+					}
+					else
+					{
+                        if (arr.Contains(y))
+                        {
+							set.Add(y);
+
+						}
+					}
+				}
+                
+			}
+			
+
+
+
+		}
+
+	}
+
+	
+}
+
+
+
