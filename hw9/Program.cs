d0f743eead6456d7816a38ffb48a4482e52932eb
Andrey <ab18021999@mail.ru>
2024-02-13 20:46:49 +0500

    hw9 ready

---
+++ hw9/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.Json;
+using System.Xml;
+using static System.Text.Json.JsonElement;
+
+namespace hw9
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+
+			string json = """{"users":{"user1":{"Name":"Ivan","Surname":"Ivanov","Age":34},"user2":{"Name":"Petr","Surname":"Petrov","Age":40}}}""";
+			using (XmlWriter writer = XmlWriter.Create("myXml.xml"))
+			{
+				Rec(json, writer);
+
+			}
+            Console.WriteLine("End");
+
+
+
+			static void Rec(string json, XmlWriter writer)
+			{
+				using (JsonDocument jDoc = JsonDocument.Parse(json))
+				{ 
+					foreach (JsonProperty item in jDoc.RootElement.EnumerateObject())
+					{
+						writer.WriteStartElement(item.Name);
+						if (isRoot(item))
+						{
+							Rec(item.Value.ToString(), writer);
+
+						}
+						else
+						{
+							writer.WriteValue(item.Value.ToString());
+						}
+						writer.WriteEndElement();
+					}
+				}
+			}
+
+
+			static bool isRoot(JsonProperty prop)
+			{
+				if (prop.Value.ValueKind == JsonValueKind.Object)
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+		}
+	}
+}
